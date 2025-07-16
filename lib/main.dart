c50937ce1e0a9248d7751979323d2c9bdb79c260
Ahmed El-Shamy <ahmedelshamy14620@gmail.com>
2025-07-16 06:16:59 +0300

    block landscape mode for mobiles and finish the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:car_control_app/features/home/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:device_preview/device_preview.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(DevicePreview(enabled: true, builder: (context) => CarControlApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(CarControlApp());
 }
 
 class CarControlApp extends StatelessWidget {
